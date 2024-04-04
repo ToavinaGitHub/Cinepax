5aa7ac36c498f8b4d6cad49652b8873a487c748e
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2024-04-04 16:02:48 +0300

    feat : Initial commit

---
+++ src/main/java/com/cinepax/mg/CinepaxApplication.java
@@ -0,0 +1,13 @@
+package com.cinepax.mg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinepaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinepaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinepax/mg/CinepaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinepax.mg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
