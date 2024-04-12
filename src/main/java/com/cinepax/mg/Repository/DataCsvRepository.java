5f01a908aec8dc4d4cdac3a6b822b191ba787a05
Zak <118508607+ToavinaGitHub@users.noreply.github.com>
2024-04-12 18:53:13 +0300

    feat : update from csv func

---
+++ src/main/java/com/cinepax/mg/Repository/DataCsvRepository.java
@@ -0,0 +1,7 @@
+package com.cinepax.mg.Repository;
+
+import com.cinepax.mg.Model.DataCsv;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DataCsvRepository extends JpaRepository<DataCsv,Integer> {
+}
